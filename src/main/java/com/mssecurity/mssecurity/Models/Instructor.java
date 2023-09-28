fd3f738a95d7d97610ec233fe8d117844201b0b1
MiguelCardona11 <04.migue.cardona@gmail.commit>
2023-09-27 21:27:40 -0500

    implementacion Modelo DanceClass, Instructor y StudentDanceClass

--- src/main/java/com/mssecurity/mssecurity/Models/Instructor.java
+++ src/main/java/com/mssecurity/mssecurity/Models/Instructor.java
@@ -1,4 +1,64 @@
 package com.mssecurity.mssecurity.Models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+
 public class Instructor {
+    @Id
+    private String _id;
+    private String name;
+    private String email;
+    private Double salary;
+    @DBRef
+    private Academy academy;
+    public Instructor() {
+    }
+
+    public Instructor(String name, String email, Double salary, Academy academy) {
+        this.name = name;
+        this.email = email;
+        this.salary = salary;
+        this.academy = academy;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public Academy getAcademy() {
+        return academy;
+    }
+
+    public void setAcademy(Academy academy) {
+        this.academy = academy;
+    }
 }
--- src/main/java/com/mssecurity/mssecurity/Models/StudentDanceClass.java
+++ src/main/java/com/mssecurity/mssecurity/Models/StudentDanceClass.java
@@ -1,4 +1,46 @@
 package com.mssecurity.mssecurity.Models;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
+
 public class StudentDanceClass {
+    @Id
+    private String _id;
+    @DBRef
+    private Student student_id;
+    @DBRef
+    private DanceClass danceClass_id;
+
+    public StudentDanceClass(){
+    }
+
+    public StudentDanceClass(Student student_id, DanceClass danceClass_id) {
+        this.student_id = student_id;
+        this.danceClass_id = danceClass_id;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public Student getStudent_id() {
+        return student_id;
+    }
+
+    public void setStudent_id(Student student_id) {
+        this.student_id = student_id;
+    }
+
+    public DanceClass getDanceClass_id() {
+        return danceClass_id;
+    }
+
+    public void setDanceClass_id(DanceClass danceClass_id) {
+        this.danceClass_id = danceClass_id;
+    }
 }
